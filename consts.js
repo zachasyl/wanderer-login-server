9b5d4dfa61586d21fd90d8a5c8a41b1dc3263616
Nishtha Goswami <nishthagoswami@Nishthas-MBP.cable.rcn.com>
2021-12-13 21:58:37 -0500

    const change

--- consts.js
+++ consts.js
@@ -1,5 +1,6 @@
-// const MONGODB_URL = 'mongodb://localhost:27017/tuiter';
-const MONGODB_URL = 'mongodb+srv://web-dev:wandererproject@cluster0.1zdau.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+
+//const MONGODB_URL = 'mongodb://18.224.136.49/wanderer';
+const MONGODB_URL = 'mongo mongodb+srv://web-dev:wandererproject@cluster0.1zdau.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
 
 module.exports = {
   MONGODB_URL
