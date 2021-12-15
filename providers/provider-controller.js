e9a209ed9c8ca39ee9c162a2d3ec848a74a0cb4d
Sanat Deshpande <sanatdeshpande1@gmail.com>
2021-12-15 01:45:19 -0500

    fixes

--- providers/provider-controller.js
+++ providers/provider-controller.js
@@ -48,6 +48,7 @@ module.exports = (app) => {
   };
 
   const registration = (req, res) => {
+    console.log(req.body);
     providerDao.createProvider(req.body).then((user) => {
       req.session["profile"] = user;
       res.json(user);
